77df13dbf7a9f9eb0e5d16c233eee678fcfda1b2
haowaiwai <haowaiwai@hotmail.com>
2010-12-15 16:17:24 +0800

    First commit

---
+++ src/hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 10-12-15
+ * Time: 下午4:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class hello {
+}
